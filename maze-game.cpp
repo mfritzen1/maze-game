81baa19b29e7f8e104c8f473f636f50899c7e94a
mfritzen1 <masonfritzen.dev@gmail.com>
2026-03-14 15:17:44 -0400

    got it to find start and end positions

--- maze-game.cpp
+++ maze-game.cpp
@@ -4,19 +4,54 @@
 
 int main()
 {
-  std::ifstream inf{"mazes/maze-1.txt"};
+  struct coord // coordinates of a given point in the maze, [0,0] is in the top left corner
+  {
+    int x{}; // x-coordinate
+    int y{}; // y-coordinate
+  };
 
-  if (!inf)
+  int height{0};          // number of rows in maze grid
+  int width{};            // number of columns in maze grid
+  int startX{};           // x-coordinate of the starting position, used for intermediate step
+  int endX{};             // x-coordinate of the ending position, used for intermediate step
+  coord startPos{};       // coordinates of the starting position
+  coord endPos{};         // coordinates of the ending position
+  std::string strInput{}; // a given line of the maze file
+
+  // get maze file
+  std::ifstream mazeFile{"mazes/maze-1.txt"};
+
+  if (!mazeFile)
   {
     std::cerr << "maze file failed to open\n";
     return 1;
   }
 
-  std::string strInput{};
-  while (std::getline(inf, strInput))
+  // read the maze file, find coordinates of starting and ending positions
+  while (std::getline(mazeFile, strInput))
   {
     std::cout << strInput << '\n';
+    startX = strInput.find('S');
+    endX = strInput.find('E');
+    if (startX < strInput.length())
+    {
+      startPos.x = startX;
+      startPos.y = height;
+    }
+    if (endX < strInput.length())
+    {
+      endPos.x = endX;
+      endPos.y = height;
+    }
+    height++;
   }
+  width = strInput.length();
+
+  // output
+  std::cout << "height: " << height << '\n';
+  std::cout << "width: " << width << '\n';
+  std::cout << "S position: [" << startPos.x << ", " << startPos.y << "]" << '\n';
+  std::cout << "E position: [" << endPos.x << ", " << endPos.y << "]" << '\n';
 
   return 0;
 }
